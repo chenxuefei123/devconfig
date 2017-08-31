5c3d4795eead931d5f959ddad5e80492b0541215
chenxuefei123 <chen@laioffer.com>
2017-08-31 05:47:38 -0700

    laicode test config

---
+++ laicode/env.sh
@@ -0,0 +1,10 @@
+LAICODE_DB_NAME=laicode
+LAICODE_DB_USER=newuser
+LAICODE_DB_PWD=password
+LAICODE_DB_HOST=10.1.10.190
+LAICODE_DB_PORT=3306
+LAICODE_JUDGE_ENDPOINT=10.1.10.190:5001
+LAICODE_LOG_DIR=/Users/chenxuefei/code/laicode/build/work_dir
+LAICODE_CONTAINER_WORK_DIR=/var/run/laicode/work
+LAICODE_HOST_WORK_DIR=/Users/chenxuefei/code/laicode/build/work_dir
+LAICODE_HOST_DATA_DIR=/Users/chenxuefei/code/laicode/build/work_dir
---
+++ laicode/host-env.sh
@@ -0,0 +1,9 @@
+LAICODE_DB_NAME=laicode
+LAICODE_DB_USER=newuser
+LAICODE_DB_PWD=password
+LAICODE_DB_HOST=10.1.10.190
+LAICODE_DB_PORT=3306
+LAICODE_LOG_DIR=/Users/chenxuefei/code/laicode/build/work_dir
+LAICODE_HOST_WHOOSH_INDEX_DIR=/Users/chenxuefei/code/laicode/build/index
+LAICODE_CONTAINER_WORK_DIR=/var/run/laicode/work
+LAICODE_HOST_WORK_DIR=/Users/chenxuefei/code/laicode/build/work_dir
